cb2528b4a057cf43cde1b3acf1c1d45ff1a5607a
Rakesh Kumar <webmkumar@gmail.com>
2024-01-24 19:13:37 +0530

    all setup configured change

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -3,7 +3,7 @@ import fs from "fs";
 
 cloudinary.config({
   cloud_name: "process.env.CLOUDINARY_CLOUD_NAME",
-  api_key: "process.env.229571954357247",
+  api_key: "process.env.CLOUDINARY_API_KEY",
   api_secret: "process.env.CLOUDINARY_API_SECRET",
 });
 
